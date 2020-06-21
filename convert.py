7474082a5237bcfa2d62a7414f3d486b7e7fb94f
QuantumCalzone <QuantumCalzone@gmail.com>
2020-06-21 12:39:17 -0700

    Updates

--- convert.py
+++ convert.py
@@ -42,6 +42,23 @@ def delete_root_folder(folder_name):
                 print(f"delete_root_folder( folder_name: {folder_name} ) | could not find")
 
 
+def read_git_info(file_name):
+    if verbose:
+        print(f"read_git_info( file_name: {file_name} )")
+
+    active_script_path = os.path.realpath(__file__)
+    project_path = get_parent_dir(active_script_path)
+
+    git_info_path = os.path.join(project_path, f"{file_name}.txt")
+    ensure_file_path_exists(git_info_path)
+
+    git_info_file = open(git_info_path, "r")
+    git_info = git_info_file.read()
+    git_info_file.close()
+
+    return git_info
+
+
 upm_branch = repository.lookup_branch("upm")
 
 if upm_branch is not None:
@@ -65,4 +82,23 @@ os.rmdir(assets_folder_path)
 
 # https://www.google.com/search?q=pygit2+commit+all+changes&rlz=1C1CHBF_enUS838US838&oq=pygit2+commit+all+changes&aqs=chrome..69i57.4807j0j4&sourceid=chrome&ie=UTF-8
 
-# switch_branch("master")
+user_name = read_git_info("user_name")
+user_mail = read_git_info("user_mail")
+
+if user_name == "" or user_mail == "":
+    print("You need to fill out info!")
+else:
+    index = repository.index
+    index.add_all()
+    index.write()
+    author = pygit2.Signature(user_name, user_mail)
+    commiter = pygit2.Signature(user_name, user_mail)
+    tree = repository.TreeBuilder().write()
+    commit = repository.create_commit("refs/heads/upm", author, commiter, "Package", tree, [repository.head.target])
+
+    # sshcred = repository.credentials.Keypair("git", "/path/to/id_rsa.pub", "/path/to/id_rsa", "")
+    # repository.crendentals = sshcred
+    #
+    # repository.remotes["origin"].push(["refs/heads/upm:refs/heads/upm"])
+
+    # switch_branch("master")
