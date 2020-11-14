749c7574ac424845e0574aeb82ae18bdf1a18a2b
QuantumCalzone <QuantumCalzone@gmail.com>
2020-11-13 18:33:50 -0800

    idk lol

---
+++ auto_convert.py
@@ -0,0 +1,17 @@
+import convert
+import os
+
+verbose = True
+print("YES")
+
+
+def create_module():
+    if verbose:
+        print(f"create_module()")
+
+
+def convert(project_path):
+    if verbose:
+        print(f"convert( project_path: {project_path} )")
+
+    # convert.convert(project_path)
