2efd2508ec8f4fa572c893be14451305e06524d7
QuantumCalzone <QuantumCalzone@gmail.com>
2020-11-14 17:31:41 -0800

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='unity-project-to-upm',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/QuantumCalzone/unity-project-to-upm',
+    license='',
+    author='QuantumCalzone',
+    author_email='QuantumCalzone@gmail.com',
+    description='An automation tool to convert your unity project into a package hosted with git.'
+)
